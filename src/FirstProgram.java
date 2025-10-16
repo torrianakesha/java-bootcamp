c42a34ad0090d6b898c08e7d76db796ab9c843f2
Kesha Torriana <keshatorriana.work@gmail.com>
2025-10-16 16:29:24 +0800

    Initial commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("Hello, I am Kesha N. Torriana");
+    }
+}
